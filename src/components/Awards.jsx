026abc62dc954cf80553ea61f6405e2931247a8b
Rundyson <0321-3589@lspu.edu.ph>
2026-05-06 14:03:07 +0800

    Update image paths in Awards component

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -7,28 +7,28 @@ function Awards() {
 
       <div className="awards-grid">
         <div className="award-card">
-          <img src="/public/img/3rdH.png" alt="Certificate" />
+          <img src="/img/3rdH.png" alt="Certificate" />
           <div className="award-content">
             <h3>3RD HONORS S. Y. 2024 - 2025</h3>
             <span>2025</span>
           </div>
         </div>
         <div className="award-card">
-          <img src="/public/img/cmsc.jpg" alt="Certificate" />
+          <img src="/img/cmsc.jpg" alt="Certificate" />
           <div className="award-content">
             <h3>CMSC-PFSC 2025 (PHILIPPINE ASSOSCIATION FOR COMMUNICATION EDUCATORS)</h3>
             <span>2025</span>
           </div>
         </div>
         <div className="award-card">
-          <img src="/public/img/pinoy.png" alt="Certificate" />
+          <img src="/img/pinoy.png" alt="Certificate" />
           <div className="award-content">
             <h3>PINOY MEDIA CONGRESS 2025 (PHILIPPINE ASSOSCIATION FOR COMMUNICATION EDUCATORS)</h3>
             <span>2025</span>
           </div>
         </div>
         <div className="award-card">
-          <img src="/public/img/pinoy2.png" alt="Certificate" />
+          <img src="/img/pinoy2.png" alt="Certificate" />
           <div className="award-content">
             <h3>PINOY MEDIA CONGRESS 2025 (PHILIPPINE ASSOSCIATION FOR COMMUNICATION EDUCATORS)</h3>
             <span>2025</span>
